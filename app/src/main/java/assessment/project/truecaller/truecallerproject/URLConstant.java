6ba26959ba3ff1264950bd35d50cd49096238d5f
Siminy <siminy21@yahoo.co.uk>
2019-01-02 10:08:02 +0100

    Delete URLConstant.java

--- app/src/main/java/assessment/project/truecaller/truecallerproject/URLConstant.java
+++
@@ -1,9 +0,0 @@
-package assessment.project.truecaller.truecallerproject;
-
-public class URLConstant {
-    /**
-     *  URL Constants
-     */
-  public static final String TRUE_CALLER_URL = "https://blog.truecaller.com/2018/01/22/life-as-an-android-engineer/";
-
-}
