1cf77738090bc35d9dae14033e7aaddc89c75b41
Siminy <siminy21@yahoo.co.uk>
2019-01-02 10:07:55 +0100

    Delete TrueCallerFactoryInterface.java

--- app/src/main/java/assessment/project/truecaller/truecallerproject/TrueCallerFactoryInterface.java
+++
@@ -1,8 +0,0 @@
-package assessment.project.truecaller.truecallerproject;
-
-public interface TrueCallerFactoryInterface {
-    /**
-     * Interface Object getData method declaration
-     */
-    public Object getData();
-}
